7cb38f175877b5b7729cf41b69c2edbc19db74e9
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-04 08:01:42 +0000

    Add GridPane, add label for TextField, rearrange nodes, change text font

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,11 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,9 +26,12 @@ public class Main extends Application {
 
         //create a new node
         Text text = new Text("Sup?");
+        //change the font for the text
+        text.setFont(new Font("Papyrus", 80));
         //move the new node 50px down from the top
         text.setY(50);
-
+        //create a new node
+        Label label = new Label("Name:");
         //create a new node
         TextField nameFld = new TextField();
 
@@ -35,12 +41,22 @@ public class Main extends Application {
         btn.setOnAction(event -> System.out.printf("Sup %s!%n",
                 nameFld.getText().trim()));
 
+        //create a grid layout
+        GridPane grid = new GridPane();
+        //add elements to the grid
+        grid.add(label, 0, 0);
+        grid.add(nameFld, 1,0);
+        grid.add(btn, 1, 1);
+        //set gap
+        grid.setHgap(20);
+        //see the lines of the grid
+        grid.setGridLinesVisible(true);
 
 
         //create a Vertical Box layout
         VBox box = new VBox();
         //add the nodes to the box
-        box.getChildren().addAll(text, nameFld, btn);
+        box.getChildren().addAll(text, grid);
         //add the node to the scene
         root.getChildren().add(box);
 
