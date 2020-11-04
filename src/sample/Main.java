a9fd5ef0bcacfdacaee1d940e04caef9a1caf979
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-04 08:40:07 +0000

    Separate Layout using .fxml file.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,47 +18,46 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //comment out next line because we do not want to use the FXBuilder
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        //create the root
-        Group root = new Group();
-
-        //create a new node
-        Text text = new Text("Sup?");
-        //change the font for the text
-        text.setFont(new Font("Papyrus", 80));
-        //move the new node 50px down from the top
-        text.setY(50);
-        //create a new node
-        Label label = new Label("Name:");
-        //create a new node
-        TextField nameFld = new TextField();
-
-        //create a new node
-        Button btn = new Button("Say Sup!");
-        //create an intention action for the button
-        btn.setOnAction(event -> System.out.printf("Sup %s!%n",
-                nameFld.getText().trim()));
-
-        //create a grid layout
-        GridPane grid = new GridPane();
-        //add elements to the grid
-        grid.add(label, 0, 0);
-        grid.add(nameFld, 1,0);
-        grid.add(btn, 1, 1);
-        //set gap
-        grid.setHgap(20);
-        //see the lines of the grid
-        grid.setGridLinesVisible(true);
-
-
-        //create a Vertical Box layout
-        VBox box = new VBox();
-        //add the nodes to the box
-        box.getChildren().addAll(text, grid);
-        //add the node to the scene
-        root.getChildren().add(box);
+//        //create the root
+//        Group root = new Group();
+//
+//        //create a new node
+//        Text text = new Text("Sup?");
+//        //change the font for the text
+//        text.setFont(new Font("Papyrus", 80));
+//        //move the new node 50px down from the top
+//        text.setY(50);
+//        //create a new node
+//        Label label = new Label("Name:");
+//        //create a new node
+//        TextField nameFld = new TextField();
+//
+//        //create a new node
+//        Button btn = new Button("Say Sup!");
+//        //create an intention action for the button
+//        btn.setOnAction(event -> System.out.printf("Sup %s!%n",
+//                nameFld.getText().trim()));
+//
+//        //create a grid layout
+//        GridPane grid = new GridPane();
+//        //add elements to the grid
+//        grid.add(label, 0, 0);
+//        grid.add(nameFld, 1,0);
+//        grid.add(btn, 1, 1);
+//        //set gap
+//        grid.setHgap(20);
+//        //see the lines of the grid
+//        grid.setGridLinesVisible(true);
+//
+//
+//        //create a Vertical Box layout
+//        VBox box = new VBox();
+//        //add the nodes to the box
+//        box.getChildren().addAll(text, grid);
+//        //add the node to the scene
+//        root.getChildren().add(box);
 
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275));
